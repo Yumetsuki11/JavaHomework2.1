09504310185b516b85699fa651371572383462f6
Yumetsuki11 <shar2107@yandex.ru>
2022-06-19 18:31:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int fee = 24597;
+        int feeDenominator = 20;
+
+        System.out.println("Начислено " + (fee / feeDenominator) + " миль");
+    }
+}

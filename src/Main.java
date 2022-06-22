679a2491219312826e5c63105fc2e7713acafaa5
Yumetsuki11 <shar2107@yandex.ru>
2022-06-22 17:53:56 +0300

    Refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        BonusMilesService service = new BonusMilesService();
+        int price = 24_597;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+
+        /* первоначальный вариант программы
         int fee = 24597;
         int feeDenominator = 20;
 
         System.out.println("Начислено " + (fee / feeDenominator) + " миль");
+        */
     }
 }
